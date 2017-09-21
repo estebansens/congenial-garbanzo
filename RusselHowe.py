aefd6fe95cc5123918f7e4b116d0a6a1d1501fbc
estebansens <32148745+estebansens@users.noreply.github.com>
2017-09-20 19:24:30 -0500

    Add files via upload
    A somewhat simple chatbot.

---
+++ RusselHowe.py
@@ -0,0 +1,27 @@
+#print(3+3)
+#print('hello')
+#word= 'my word'
+#print(word)
+ 
+'''This program will
+    talk back to you'''
+
+#greeting
+print ('Hello! What is your name?')
+name = input()
+print ('Well, hello there, ' + name + '.')
+print ('My name is Russel Howe, but my friends call me Russ!')
+print ('Do you have a nickname, ' + name + '?')
+nickname = input()
+print (nickname + ', that suits you well. I like it!')
+print ('What is your favorite color, ' + nickname +'?')
+color = input()
+print ('Oh, ' + color + ' is the color of my cat. What a coincidence.')
+print ('How many cats do you have, ' + nickname + '?')
+number = input()
+print (number + ' is a pretty number.')
+print ('Do you have a favorite number, ' + nickname + '?')
+number = input()
+print (number + ', that is nice!')
+print ('Me, myself, I have always been fond of the number 100000011')
+print ('It has been nice talking to you ' + nickname + '. Have a great day!')
